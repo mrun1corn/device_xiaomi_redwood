44fa4e3b2bba48786ed4edef7925aed04fee79d3
Dhina17 <dhinalogu@gmail.com>
2025-03-02 04:48:50 -0500

    redwood: Implement Torch Light Brightness Control
    Change-Id: I3646affbea7537954b3844a6572a09e221b15811

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,6 +214,9 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
 
+# Torch control ##
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.redwood
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
