62de78226eba8905cbc51e8560f28cea61debb10
thepriyanshujangid <priyanshujangid@yahoo.com>
2025-05-13 21:22:44 +0600

    redwood: Build android.hardware.graphics.allocator@3.0 to system * E vndksupport: Could not load /vendor/lib64/libmialgoengine.so from sphal namespace: dlopen failed: library "android.hardware.graphics.allocator@3.0.so" not found: needed by /vendor/lib64/libmialgoengine.so in namespace sphal.

--- device.mk
+++ device.mk
@@ -143,6 +143,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.allocator@3.0 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.composer-service.rc \
